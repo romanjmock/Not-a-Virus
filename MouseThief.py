c45937a55c3da810c74460174fdb037c201b63a0
romanjmock <romanjmock@gmail.com>
2024-01-28 00:53:56 -0500

    Initial Upload
    turned project into a repository

---
+++ MouseThief.py
@@ -0,0 +1,110 @@
+import tkinter
+from PIL import ImageTk, Image, ImageOps
+import threading
+from time import sleep
+from pynput import mouse
+import math
+
+#sleep(5)
+
+class MouseTheif:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.window.config(
+            bg = '#add123'
+        )
+        self.caught = False
+        width = self.window.winfo_screenwidth()
+        height = self.window.winfo_screenheight()
+        print(f'{width}, {height}')
+        self.window.geometry((str)(width) + 'x' + (str)(height))
+        #self.window.geometry((str)(width) + 'x' + (str)(height))
+        self.window.geometry('+0+0')
+        self.window.overrideredirect(True)
+        self.window.wm_attributes('-transparentcolor', '#add123')
+        self.window.wm_attributes('-topmost', True)
+
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width = width,
+            height = height,
+            highlightthickness = 0,
+            bg = '#add123'
+        )
+        self.canvas.place(
+            x = 0,
+            y = 0
+        )
+        file = Image.open('mouse.png')
+        x, y = file.size
+        mL = ImageTk.PhotoImage(file)
+        mI = self.canvas.create_image(width / 2, 85, image = mL)
+
+        self.c = mouse.Controller()
+
+        self.wX = 0
+        self.wY = 0
+        self.stealX = 0
+        self.time = 0
+
+        file = Image.open('Robber.png')
+        file = file.resize((300, 300))
+        self.file1 = ImageOps.mirror(ImageOps.flip(file))
+        self.file = ImageOps.mirror(file)
+
+        self.render()
+        self.window.mainloop()
+    def render(self):
+        x, y = self.c.position
+        angle = math.atan2(y - self.wY - 150, x - self.wX - 150)
+        hit = False
+        if self.wX + 150 < x + 10 and self.wX + 150 > x - 10 and self.wY + 150 < y + 10 and self.wY + 150 > y - 10:
+            hit = True
+        else:
+            #print(math.degrees(angle))
+            self.wX += math.cos(angle) * 5
+            self.wY += math.sin(angle) * 5
+            self.wX = (int)(self.wX)
+            self.wY = (int)(self.wY)
+            print(f'{x}, {self.wX + 150}, {y}, {self.wY + 150}')
+        #self.window.geometry('+' + (str)(self.wX) + '+' + (str)(self.wY))
+        '''self.canvas.place(
+            x = self.wX,
+            y = self.wY
+        )'''
+        if math.degrees(angle) > -90 and math.degrees(angle) < 90:
+            file = self.file.copy().rotate(math.degrees(-angle))
+        else:
+            file = self.file1.copy().rotate(math.degrees(-angle))
+        img = ImageTk.PhotoImage(file)
+        i = self.canvas.create_image(self.wX + 150, self.wY + 150, image = img)
+        self.canvas.image = img
+        if hit == False:
+            self.window.after(10, self.render)
+        elif hit == True:
+            self.canvas.delete(tkinter.ALL)
+            self.render1()
+    def render1(self):
+        self.caught = True
+        self.c.position = (self.wX - self.stealX + 378 * (300 / 512), self.wY + 312 * (300 / 512))
+        img = ImageTk.PhotoImage(self.file1.rotate(180))
+        i = self.canvas.create_image(self.wX + 150 - self.stealX, self.wY + 150, image = img)
+        self.canvas.image = img
+        self.stealX += 5
+        self.time += 1
+        if self.wX - self.stealX + 300 >= 0:
+            self.window.after(10, self.render1)
+    def getCtrl(self):
+        return self.c
+
+c = MouseTheif()
+
+def onMove(data, mx, my):
+    if (c.caught == True):
+        c.c.position = (c.wX - c.stealX + 378 * (300 / 512), c.wY + 312 * (300 / 512))
+    print('moving', mx, my)
+
+l = mouse.Listener(on_move = onMove)
+l.start()
+
+
---
+++ ScopeIn.py
@@ -0,0 +1,94 @@
+import tkinter
+from PIL import Image, ImageTk
+from pynput import mouse
+import screeninfo
+
+window = tkinter.Tk()
+width = screeninfo.get_monitors()[0].width
+height = screeninfo.get_monitors()[0].height
+#width = window.winfo_screenwidth()
+#height = window.winfo_screenheight()
+window.geometry((str)(width) + 'x' + (str)(height))
+window.geometry('+0+0')
+
+window.overrideredirect(True)
+window.wm_attributes('-transparentcolor', '#202020')
+window.wm_attributes('-topmost', True)
+window.config(
+    bg = '#202020'
+)
+
+c = tkinter.Canvas(
+    window,
+    width = width,
+    height = height,
+    bg = '#202020',
+    highlightthickness = 0
+)
+c.place(
+    x = 0,
+    y = 0
+)
+bulletDamage = Image.open('bulletDamage.png')
+bulletDamage = bulletDamage.resize((200, 200), False)
+bulletDamage = ImageTk.PhotoImage(bulletDamage)
+bullets = []
+def onClick(mx, my, key, click):
+    global bulletDamage
+    print(mx, my, key, click, width, height)
+    if ((str)(key) == "Button.left" or (str)(key) == "Button.right"):
+        if (click == True):
+            bullets.append((mx, my))
+            m.click()
+            m.move(0, -50)
+
+m = mouse.Controller()
+m1 = mouse.Listener(on_click = onClick)
+m1.start()
+
+sniper = Image.open('Sniper.png')
+sniper = sniper.resize((400, 400))
+sniper = ImageTk.PhotoImage(sniper)
+
+sign = Image.open('HuntingSeason.png')
+sign = sign.crop((0, 70, 400, 330))
+sign = sign.resize((500, 300))
+sign = ImageTk.PhotoImage(sign)
+
+time = 0
+def printImage():
+    global width, height, time
+    x, y = m.position
+    x += 5
+    y += 5
+    c.delete(tkinter.ALL)
+    for j in range (len(bullets)):
+        i = c.create_image(
+            bullets[j][0],
+            bullets[j][1],
+            image = bulletDamage
+        )
+        label = tkinter.Label(image = bulletDamage)
+        label.image = bulletDamage
+    s = c.create_image(
+        x,
+        y,
+        image = sniper
+    )
+    r = c.create_rectangle(0, 0, x - 200, height, fill = '#000000')
+    r1 = c.create_rectangle(x + 200, 0, width, height, fill = '#000000')
+    r2 = c.create_rectangle(x - 200, 0, x + 200, y - 200, fill = '#000000')
+    r3 = c.create_rectangle(x - 200, y + 200, x + 200, height, fill = '#000000')
+
+    s1 = c.create_image(
+        width / 2,
+        120,
+        image = sign
+    )
+    time += 1
+    if time == 1000:
+        exit(0)
+    window.after(10, printImage)
+
+printImage()
+window.mainloop()
