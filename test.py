10bcdb4eedef230f8d6f3961c32ce3583b2509cd
romanjmock <romanjmock@gmail.com>
2024-01-31 10:50:13 -0500

    fixed GII file

--- test.py
+++ test.py
@@ -7,24 +7,21 @@ import psutil
 path = "C:/users/" + os.getlogin() + "/AppData/Local/Programs/Python/Python312/python.exe "
 
 invade = path + 'C:/Users/Public/Kernal46/PythonBased/Invade.py'
-print('invade is now', invade)
+#print('invade is now', invade)
 mouseThief = path + 'C:/Users/Public/Kernal46/PythonBased/MouseThief.py'
 scopeIn = path + 'C:/Users/Public/Kernal46/PythonBased/ScopeIn.py'
 print(invade)
 
+print('step 1')
 CREATE_NO_WINDOW = 0x08000000
-
-sleep(1)
+#sleep(1)
+print('starting')
 while True:
+    #print('count is currently', count)
     r = (int)(random.random() * 3)
     r = 0
     if (r == 0):
         print('invade')
         subprocess.call(invade, creationflags = CREATE_NO_WINDOW)
-    if (r == 1):
-        print('mouseThief')
-        subprocess.call(mouseThief, creationflags = CREATE_NO_WINDOW)
-    if (r == 2):
-        print('scopeIn')
-        subprocess.call(scopeIn, creationflags = CREATE_NO_WINDOW)
-    sleep(1)
+    sleep(.1)
+    print('starting again')
